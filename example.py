3365dfc473c690a363788e95b3c09338a774b79f
Djura Smits <djura.smits@gmail.com>
2021-10-07 12:17:42 +0200

    Added example.py

---
+++ example.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add('space', 'ship') == 'spaceship'
+
+
+def subtract(a, b):
+    return a + b  # <--- fix this 
+
+
+# uncomment the following test
+#def test_subtract():
+#    assert subtract(2, 3) == -1
